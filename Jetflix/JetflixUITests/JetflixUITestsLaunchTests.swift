e7be31a16d3794f933f2d8853747ee4795c8d797
유정주 <dbwjdwn9216@gmail.com>
2023-05-12 21:48:51 +0900

    프로젝트 init

---
+++ Jetflix/JetflixUITests/JetflixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JetflixUITestsLaunchTests.swift
+//  JetflixUITests
+//
+//  Created by 유정주 on 2023/05/12.
+//
+
+import XCTest
+
+final class JetflixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
