752a736391d633bc927615226f6be532c2fe21a5
유정주 <dbwjdwn9216@gmail.com>
2023-05-13 15:53:22 +0900

    에피소드 1 완료
    https://www.youtube.com/watch?v=LWGr9fQR498&list=PLqj8V2bxPxpDRRpKl01DS5qc3ImXb_MYg&index=1

---
+++ Jetflix/Jetflix/Presentation/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Jetflix
+//
+//  Created by 유정주 on 2023/05/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+}
---
+++ Jetflix/Jetflix/Presentation/Search/ViewController/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Jetflix
+//
+//  Created by 유정주 on 2023/05/13.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
