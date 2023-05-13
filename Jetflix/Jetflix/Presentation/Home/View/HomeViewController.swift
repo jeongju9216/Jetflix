2ba04e7a7a744b40411f2e34766709d156f6200a
유정주 <dbwjdwn9216@gmail.com>
2023-05-13 18:59:32 +0900

    에피소드4 완료

--- Jetflix/Jetflix/Presentation/Home/View/HomeViewController.swift
+++ Jetflix/Jetflix/Presentation/Home/View/HomeViewController.swift
@@ -12,9 +12,15 @@ class HomeViewController: UIViewController {
     //MARK: - Views
     private let homeFeedTable: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = .systemBackground
+        
         tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        
         return tableView
     }()
+    
+    //MARK: - Properties
+    let sectionTitles: [String] = ["Trending Movies", "Popular", "Trending TV", "Upcoming Movies", "Top rated"]
     
     //MARK: - Life Cycles
     override func viewDidLoad() {
@@ -26,6 +32,8 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
+        configurationNavbar()
+        
         let headerView = PosterHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
     }
@@ -34,6 +42,29 @@ class HomeViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         homeFeedTable.frame = view.bounds
+    }
+    
+    //MARK: - Methods
+    private func configurationNavbar() {
+        var image = UIImage(named: "LogoImage")
+        //iOS 시스템에서 색상을 변경하는 것을 막고, 항상 이미지 본연의 색상으로 출력하게 함
+        image = image?.withRenderingMode(.alwaysOriginal)
+        
+        let logoItem = UIButton(type: .custom)
+        logoItem.frame = CGRect(x: 0.0, y: 0.0, width: 32, height: 32)
+        logoItem.setImage(image, for: .normal)
+
+        let leftItem = UIBarButtonItem(customView: logoItem)
+        leftItem.customView?.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        leftItem.customView?.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        navigationItem.leftBarButtonItem = leftItem
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil),
+        ]
+        
+        navigationController?.navigationBar.tintColor = .label
     }
 }
 
@@ -54,15 +85,36 @@ extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        //아래로 스크롤을 하면 네비게이션바가 함께 올라가도록 구현
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
 }
 
 //MARK: - UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.textColor = .label
+        header.textLabel?.text = header.textLabel?.text?.capitalized
     }
 }
