53bdfe862ed406467ab6cf3d7fa8d96faae903aa
유정주 <dbwjdwn9216@gmail.com>
2023-05-13 17:19:23 +0900

    에피소드 2 완료
    https://www.youtube.com/watch?v=rp_wJMoKu5o&list=PLqj8V2bxPxpDRRpKl01DS5qc3ImXb_MYg&index=2

---
+++ Jetflix/Jetflix/Presentation/Home/View/HomeViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HomeViewController.swift
+//  Jetflix
+//
+//  Created by 유정주 on 2023/05/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    //MARK: - Views
+    private let homeFeedTable: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return tableView
+    }()
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        homeFeedTable.frame = view.bounds
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
