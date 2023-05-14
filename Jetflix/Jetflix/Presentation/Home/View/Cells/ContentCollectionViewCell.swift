60b50c56addd8d30bfa63f42e907cbe3d355f6a3
유정주 <dbwjdwn9216@gmail.com>
2023-05-14 11:37:12 +0900

    에피소드 7 완료
    https://www.youtube.com/watch?v=SOSX_ENJtFg

---
+++ Jetflix/Jetflix/Presentation/Home/View/Cells/ContentCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  ContentCollectionViewCell.swift
+//  Jetflix
+//
+//  Created by 유정주 on 2023/05/14.
+//
+
+import UIKit
+import Kingfisher
+
+class ContentCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ContentCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    func configure(with urlString: String) {
+//        print("\(#function) urlString: \(urlString)")
+//        posterImageView.setImageUsingJIC(url: "https://image.tmdb.org/t/p/w500" + urlString)
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500" + urlString) else {
+            return
+        }
+        
+        posterImageView.kf.setImage(with: url)
+    }
+}
