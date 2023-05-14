d2b4674d4d07267c0ba5f1951c8329a0f187c5c0
유정주 <dbwjdwn9216@gmail.com>
2023-05-14 14:42:24 +0900

    에피소드 9 완료
    https://www.youtube.com/watch?v=ni4E1Gdie-c

--- Jetflix/Jetflix/Presentation/Search/View/SearchViewController.swift
+++ Jetflix/Jetflix/Presentation/Search/View/SearchViewController.swift
@@ -9,9 +9,72 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    //MARK: - Views
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(ContentTableViewCell.self, forCellReuseIdentifier: ContentTableViewCell.identifier)
+        return table
+    }()
+    
+    //MARK: - Properties
+    let repository = ContentRepository()
+    private var movies: [Movie] = []
+    
+    //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "Top Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        discoverTable.frame = view.bounds
+    }
+    
+    //MARK: - Methods
+    private func fetchData() {
+        Task {
+            do {
+                movies = try await repository.getDiscoverMovies()
+                discoverTable.reloadData()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContentTableViewCell.identifier, for: indexPath) as? ContentTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(with: movies[indexPath.row])
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
 }
