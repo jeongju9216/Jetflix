993e8666f8183944242a9c35d701003858b1e4bd
유정주 <dbwjdwn9216@gmail.com>
2023-05-14 14:13:00 +0900

    홈 TableHeaderView 디자인 수정

--- Jetflix/Jetflix/Presentation/Home/View/PosterHeaderUIView.swift
+++ Jetflix/Jetflix/Presentation/Home/View/PosterHeaderUIView.swift
@@ -12,12 +12,24 @@ class PosterHeaderUIView: UIView {
     //MARK: - Views
     private let posterImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
-        imageView.image = UIImage(named: "PosterImage")
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .top
         imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
         
         return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+                
+        return label
     }()
     
     private let playButton: UIButton = {
@@ -55,9 +67,11 @@ class PosterHeaderUIView: UIView {
         
         addSubview(posterImageView)
         addGradient()
+        addSubview(titleLabel)
         
         addSubview(playButton)
         addSubview(downloadButton)
+        
         applayConstraints()
     }
     
@@ -71,21 +85,40 @@ class PosterHeaderUIView: UIView {
     }
     
     //MARK: - Methods
+    func configure(with movie: Movie) {
+        guard let posterURL = movie.posterURL, let url = URL(string: posterURL) else {
+            posterImageView.image = UIImage(named: "PosterImage")
+            return
+        }
+        
+        posterImageView.kf.setImage(with: url)
+        titleLabel.text = movie.displayTitle
+    }
+    
     private func applayConstraints() {
         let playButtonConstraints = [
             playButton.widthAnchor.constraint(equalToConstant: bounds.width * 0.42),
+            playButton.heightAnchor.constraint(equalToConstant: 40),
             playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50)
         ]
         
         let downloadButtonConstraints = [
             downloadButton.widthAnchor.constraint(equalToConstant: bounds.width * 0.42),
+            downloadButton.heightAnchor.constraint(equalToConstant: 40),
             downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50)
         ]
         
+        let titleLabelConstraints = [
+            titleLabel.widthAnchor.constraint(equalToConstant: bounds.width * 0.8),
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: playButton.topAnchor, constant: -20),
+        ]
+        
         NSLayoutConstraint.activate(playButtonConstraints)
         NSLayoutConstraint.activate(downloadButtonConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
     }
     
     private func addGradient() {
